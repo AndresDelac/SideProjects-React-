cc142a6262a318df882f6a15fc766838a08d3686
Andres De la Cruz <hugoandres2205@gmail.com>
2024-10-15 10:09:59 -0500

    terminadooo 2

--- cotizador/src/components/Resultado.jsx
+++ cotizador/src/components/Resultado.jsx
@@ -5,7 +5,7 @@ const Resulatdo = ({ total, cantidad, plazo }) => {
             <h2>Cotizacion:</h2>
             <p>La cantidad solicitada: $ {cantidad}</p>
             <p>A pagar en un plazo de: {plazo} meses</p>
-            <p>Su pago mensual es de: $ {total / plazo}</p>
+            <p>Su pago mensual es de: $ {(total / plazo).toFixed(2)}</p>
             <p>Total a pagar : $ {total}</p>
         </div>
      );
