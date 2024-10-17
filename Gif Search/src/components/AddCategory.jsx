8892d528ab1b4e16c806c40222d0458b63157aa1
Andres De la Cruz <hugoandres2205@gmail.com>
2024-10-17 11:17:11 -0500

    primero

--- Gif Search/src/components/AddCategory.jsx
+++ Gif Search/src/components/AddCategory.jsx
@@ -1,17 +1,33 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
 const AddCategory = ({ setCategory }) => {
     
     const [value, setValue] = useState("") 
+    const [error, setError] = useState(false) 
     
+
+    const searchGif = e => {
+        e.preventDefault();
+        if (value === "") {
+            setError(true);
+            return;
+        }
+        setError(false);
+        setCategory(value);
+        console.log("Enter search");
+        
+    }
+
     return ( 
         <>
-        <h2>ola</h2>
-        <form action="">
+        <form onSubmit={searchGif}>
             <input type="text" placeholder="Search..."
                 onChange={e => setValue(e.target.value)}
             />
         </form>
+        { error ? <p className="error">El campo no puede estar vacio...</p> : "" }
+        
         </>
      );
 }
