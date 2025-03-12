e0a752975957505f4b536e1ccdda3e7259b6996e
Andres De la Cruz <hugoandres2205@gmail.com>
2025-03-12 17:33:30 -0500

    chat-react v.1.4.0

--- chat-react/src/components/SendMessage.jsx
+++ chat-react/src/components/SendMessage.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db, auth } from "../firebase";
-import EmojiPicker from 'emoji-picker-react';
 const SendMessage = () => {
-    const [ input, setInput ] = useState();
-    const [ open, setOpen ] = useState("close");
+    const [ input, setInput ] = useState("");
 
     const sendMessage = async e =>{
         e.preventDefault()
@@ -19,34 +17,11 @@ const SendMessage = () => {
     setInput("");
 }
 
-    const emoji = () => setOpen("open");
-    const closeEmoji = () => setOpen("close");
-    const onEmojiClick = (event, emojiObject) => {
-        console.log(emojiObject);
-        setInput(`${input}${emojiObject}`);
-    };
+
 
     return ( 
         <form onSubmit={sendMessage}>
             
-            <button
-                type="button"
-                className="btn-emoji"
-                onClick={emoji}
-            >
-                <i className="fa-regular fa-face-smile"></i>
-            </button>
-
-            <div className={open}>
-            <button
-                type="button"
-                className="close-emoji"
-                onClick={closeEmoji}
-            >
-                <i className="fa-solid fa-x"></i>
-            </button>
-            <EmojiPicker />
-            </div>
 
             <input type="text"
             placeholder="Escribe aqui!"
--- chat-react/src/components/helpers/index.js
+++ chat-react/src/components/helpers/index.js
@@ -1,14 +1,17 @@
+export const formatDate = (objDate) => {
+    if (!objDate || !objDate.seconds) {
+        return "Hace un momento";
+    }
 
-export const formatDate = objDate => {
     const date = new Date(objDate.seconds * 1000);
 
-    const time = `${date.getHours()} : ${date.getMinutes()}`
+    const time = `${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}`;
 
     const options = {
         month: 'long',
         day: 'numeric'
-    }
+    };
 
-    const newDate = date.toLocaleDateString('sp-SP', options);
-    return `${newDate} - ${time}` ;
-}
+    const newDate = date.toLocaleDateString('es-ES', options); // Cambiado a 'es-ES' para español
+    return `${newDate} - ${time}`;
+};
